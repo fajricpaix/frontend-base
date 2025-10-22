71039620a695a62d4e7011d9b2713825904e7575
omstephen <omstephen@lidowin.com>
2025-10-22 15:27:49 +0700

    input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { ThemeToggle } from "@/components/theme/ThemeToggle";
 import ButtonContent from "@/content/ButtonContent";
-import InputContent from "@/content/InputContent";
 
 export default function Home() {
   return (
@@ -8,17 +7,13 @@ export default function Home() {
       <section className="container px-5 lg:px-8">
         <div className="flex justify-between items-center mb-8">
           <h1 className="text-3xl font-semibold">Frontend Base Components</h1>
-          <ThemeToggle />
+          {/* <ThemeToggle /> */}
         </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 my-8">
           {/* Button Content */}
           <div>
             <ButtonContent />
-          </div>
-          {/* Input Content */}
-          <div>
-            <InputContent />
           </div>
         </div>
       </section>
